2026c34f49a05213b1a582e1fb829ed91f81db37
Andrew Basiuk <andrii.basiuk@mgid.com>
2019-07-15 10:58:40 +0300

    connect bootstrap

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <myComponent msg="Hello"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myComponent from './components/myComponent.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    myComponent
+  },
+  data: {
+    msg: "HELLO"
   }
 }
 </script>
---
+++ vue-cli/src/components/myComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-alert show>{{ msg }}</b-alert>
+</template>
+
+<script>
+export default {
+    name: "myComponent",
+    props: {
+        msg: String
+    }
+}
+</script>
+
