596dadec657056d72a187ead6f80d30cd576715b
Andrew Basiuk <andrii.basiuk@mgid.com>
2019-07-19 18:10:20 +0300

    commit

--- vue-cli/src/components/weather-app/DayList.vue
+++ vue-cli/src/components/weather-app/DayList.vue
@@ -1,6 +1,9 @@
 <template>
     <ul class="day-list">
-        <!-- <li>{{info}}</li> -->
+        <li v-for="(day, index) in dataArr" :key="index">
+            <div>{{ dates[index]["day"] }}</div>
+            <div>{{ temperatures[index]["tempMin"] }}</div>
+        </li>
     </ul>
 </template>
 
@@ -8,7 +11,55 @@
 export default {
     name: "DayList",
     props: {
-        info: {}
+        dataArr: Array
+    },
+    data() {
+        return {
+            dates: [],
+            temperatures: {}
+        }
+    },
+    computed: {
+        getDate() {
+            let arr = []; 
+
+            this.dataArr.forEach(element => {
+                let fullDate = element.dt_txt.split(" ")[0],
+                    fullDateArr = fullDate.split("-");
+                
+                arr.push(
+                    {
+                        day: fullDateArr[2],
+                        month: fullDateArr[1],
+                        year: fullDateArr[0]    
+                    }
+                );
+            });
+
+            return arr;
+        },
+        getTemp() {
+            let arr = [];
+
+            this.dataArr.forEach(element => {
+
+                this.dataArr.forEach(element => {
+                    arr.push(
+                        {
+                            temp: element.main.temp,
+                            tempMin: element.main.temp_min,
+                            tempMax: element.main.temp_max    
+                        }
+                    );
+                });
+            });
+
+            return arr;
+        }
+    },
+    mounted() {
+        this.dates = this.getDate;
+        this.temperatures  = this.getTemp;
     }
 }
 </script>
--- vue-cli/src/components/weather-app/WeatherApp.vue
+++ vue-cli/src/components/weather-app/WeatherApp.vue
@@ -7,10 +7,10 @@
         >
         
         <DayList v-if="showList"
-                 :info="info"
+                 :dataArr="sortDays"
         />
         <!-- <div >{{info}}</div> -->
-        <button @click="buttonClick">Click</button>
+        <!-- <button @click="buttonClick">Click</button> -->
             
     </div>
 </template>
@@ -32,23 +32,46 @@ export default {
             city: "London",
             showList: false,
             info: {},
+            sortDays: [],
             axios: require('axios')
         }
     },
     methods: {
-        buttonClick() {
-            console.log(this.info);
-        },
+        // buttonClick() {
+        //     // this.sortDays = this.sortObj;
+        //     console.log(this.sortDays);            
+        // },
         submit() {
             this.axios.get('http://api.openweathermap.org/data/2.5/forecast?q=' + this.city + '&appid=fc3da5f655d9b4c55ce7786120594255&units=metric')
-                .then(response => (this.info = response.data, console.log(this.info)));
+                .then(response => (this.info = response.data))
+                .then(() => {
+                    this.showList = true; 
+                    this.sortDays = this.sortObj;
+                });
+        }
+    },
+    computed: {
+        sortObj() {
+            let allList = this.info.list,
+                time = "15:00:00",
+                days = []; 
+            
+            allList.forEach(function(item) {
+                if (item.dt_txt.indexOf(time) != -1) {
+                    days.push(item);
+                }
+            });
+            
+            return days;
         }
     },
     mounted() {
         this.axios.get('http://api.openweathermap.org/data/2.5/forecast?q=London&appid=fc3da5f655d9b4c55ce7786120594255&units=metric')
-            .then(response => (this.info = response.data));
-
-        this.showList = true;
+            .then(response => (this.info = response.data))
+            .then(() => {
+                this.showList = true; 
+                this.sortDays = this.sortObj;
+            });
     }
 }
 </script>
